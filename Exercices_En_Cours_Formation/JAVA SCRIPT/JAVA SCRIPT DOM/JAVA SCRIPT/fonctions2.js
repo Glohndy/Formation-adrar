99a3ab989aaccedaa1670befbd1dd8b9bc468468
Glohndy <140169176+Glohndy@users.noreply.github.com>
2023-10-24 16:32:58 +0200

    adrar

--- Exercices_En_Cours_Formation/JAVA SCRIPT/JAVA SCRIPT DOM/JAVA SCRIPT/fonctions2.js
+++ Exercices_En_Cours_Formation/JAVA SCRIPT/JAVA SCRIPT DOM/JAVA SCRIPT/fonctions2.js
@@ -369,6 +369,25 @@ Ajouter le paragraphe à la div (id = tasks)
 -Créer une fonction reload qui va à chaque clic sur le bouton recharger la page.
 
 
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./script.js" async></script>
+    <title>Exercice 18 DOM</title>
+</head>
+<body>
+    <h1>Liste des tâches :</h1>
+    <label for="task">Saisir le titre de la tâche</label>
+    <input type="text" name="task" id="task">
+    <input type="button" value="Ajouter" onclick="addTask()">
+    <input type="button" value="Supprimer Tout" onclick="delAllTask()">
+    <input type="button" value="Recharger" onclick="reload()">
+    <div id="tasks"></div>
+</body>
+</html>
+
+
 //Ajouter
 function addTask(){
     const liste = document.getElementById('tasks');
@@ -407,7 +426,272 @@ function reload(){
 
 
 
+Exemple de code JS CSS :
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./script.js" async></script>
+    <link rel="stylesheet" href="./style.css">
+    <title>DOM</title>
+</head>
+<body>
+    <div id="container" class="test"></div>
+    <input type="text" name="nom">
+    <button onclick="change()">recuperer</button>
+</body>
+</html>
+
+
+//CSS
+.test{
+    background-color: brown;
+}
+.test2{
+    background-color: yellow;
+}
+
+//JS
+const container = document.querySelector('#container');
+container.textContent = "Exemple";
+const titre = document.createElement('h1');
+titre.textContent = 'Mon super titre';
+container.append(titre);
+let statut = true;
+function change(){
+    if(statut){
+        container.classList.add('test2');
+        container.classList.remove('test');
+        statut = false;
+    }else{
+        container.classList.add('test');
+        container.classList.remove('test2');
+        statut = true;
+    }  
+}
 
 
 
 
+
+
+
+//JS
+//</meta>Exemple code récupération variable :
+//récupérer la valeur d'une variable CSS
+function getCssVariable(attribut){
+    const rootAttr = getComputedStyle(root)
+    return rootAttr.getPropertyValue(attribut);
+}
+//fonction pour modifier la valeur d'une variable
+function setCssValue(attribut, value){
+    root.style.setProperty(attribut, value);
+}
+setCssValue('--bg', 'yellow');
+//CSS
+:root{
+    --bg : red;
+    --fontFamily : 'Arial';
+    --heiht : '12px';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Exercice 18 DOM : AVEC BONUS
+En vous inspirant de l'exemple ci-dessus :
+Partie HTML :
+-un titre h1 = liste des tâches,
+-un input de type text (id= task),
+-un bouton (id = add, onclick = addTask()) texte Ajouter,
+-un bouton (id = delAllTask, onclick = delAllTask()) texte Tout Supprimer,
+-un bouton (id = reload, onclick = reload()) texte Recharger la page,
+-une div (id = tasks)
+Partie JS :
+-Créer une fonction addtask qui va ajouter à chaque clic sur le bouton une nouvelle tache à la div (id = task),:
+-Récupérer la div (tasks)
+Récupérer la valeur de l'input (id task),
+-Créer un paragraphe,
+Ajouter la valeur de l'input (id task)  au paragraphe,
+Ajouter le paragraphe à la div (id = tasks)
+-Créer une fonction delAllTask qui va à chaque clic sur le bouton supprimer tous les enfants (child) contenu dans la div (tasks),
+-Créer une fonction reload qui va à chaque clic sur le bouton recharger la page. 
+Bonus :
+-Dans la fonction addTask (remplacer le code précédent) :
+A chaque clic sur la fonction : 
+      -ajouter une div (class = container),
+          -> à l'intérieur de la div pour aller ajouter un paragraphe,
+          -> récupérer le contenu de l'input texte (task) et passer la valeur au paragraphe,
+          -> ajouter un bouton (id = delete, onclick = deleteTask(this),
+          -> ajouter un bouton (id = update, onclick = updateTask(this),
+          ->Ajouter une fonction deleteTask qui va :
+             Supprimer la tache sélectionnée,
+          -Ajouter une fonction updateTask qui va :
+            Mettre à jour le nom de la tache depuis l'input du formulaire (id = task) 
+
+
+REPONSES
+//récupération de la div (tasks)
+const liste = document.querySelector('#tasks');
+//fonction pour ajouter une tâche
+function addTask(){
+    //récupération de l'input (task)
+    const valeur = document.querySelector('#task').value;
+    //création du container (container)
+    const container = document.createElement('div');
+    //ajout de l'attribut class : container (div container)
+    container.setAttribute('class', 'container');
+    //création du paragraphe
+    const paragraphe = document.createElement('p');
+    paragraphe.textContent = valeur;
+    const btUpdate = document.createElement('button');
+    //création du bouton update
+    btUpdate.textContent = "Update";
+    btUpdate.setAttribute('onclick','updateTask(this)');
+    btUpdate.setAttribute('id','update');
+    //création du bouton delete
+    const btDelete = document.createElement('button');
+    btDelete.textContent = "Delete";
+    btDelete.setAttribute('onclick','deleteTask(this)');
+    //ajouter les enfants à la div (container)
+    container.appendChild(paragraphe);
+    container.appendChild(btUpdate);
+    container.appendChild(btDelete);
+    //ajouter la div container à liste
+    liste.appendChild(container);
+}
+//fonction qui va supprimer tous les enfants de tasks
+function delAllTask(){
+    while(liste.firstChild){
+        liste.removeChild(liste.firstChild);
+    }
+}
+//fonction pour recharger la page
+function reload(){
+    location.reload();
+}
+//fonction pour supprimer la tache (div container parente)
+function deleteTask(e){
+    e.parentNode.remove();
+}
+
+//function pour mettre à jour le texte de la tache courante
+function updateTask(e){
+    //récupération de l'input (task)
+    const valeur = document.querySelector('#task').value;
+    //test si valeur est différent de vide
+    if(valeur != ""){
+        //modifier depuis le parent -> 1 enfant
+        e.parentNode.firstChild.textContent = valeur;
+        //V2 modifier depuis le parent 1 élément de la nodelist (HTML Collection)
+        //e.parentNode[0].textContent = valeur;
+        //V3 modifier depuis l'élément frére ou soeur précédent
+        //e.previousElementSibling.textContent = valeur;
+    }
+    //test si valeur est vide (redonne la même valeur)
+    else{
+        e.parentNode.firstChild.textContent = e.parentNode.firstChild.textContent
+    }
+}
+
+
+
+
+
+
+
+
+//</head>Exercice 19 DOM :
+Depuis l'exercice 18 précédent modifier la fonction updateTask :
+-Ajouter une variable (let)  statut qui va contenir un boolean => true
+elle va effectuer le traitement suivant (updateTask):
+-ajouter une condition qui va tester la valeur de la variable statut :
+       -> si statut est égal à true (if)
+                -1 récupérer la valeur du paragraphe,
+                -2 créer un input de type texte,
+                -3 remplacer le paragraphe par l'input précédemment créé, (replaceChild(nouvel élément, enfant)
+                -4 assigner la valeur (1) à l'input,
+                -5 passer statut à false,
+      -> si statut est égal à false (else) :
+                -1 récupérer la valeur de l'input (value),
+                -2 créer un paragraphe,
+                -3 remplacer l'input par le paragraphe replaceChild (paragraphe, enfant (input),
+                -4 assigner au paragraphe la valeur (1),
+                -5 passer statut à true,
+
+REPONSES
+//function pour mettre à jour le texte de la tache courante
+function updateTask(e){
+    //test si statut est égale à true
+    if(statut){
+        //récupération de la valeur de la tâche (paragraphe)
+        const valeur = e.parentNode.firstChild.textContent;
+        //création de l'élément input
+        const input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        //remplacer le paragraphe par un input
+        e.parentNode.replaceChild(input, e.parentNode.firstChild);
+        //assigner la valeur
+        e.parentNode.firstChild.value = valeur;
+        //changer la valeur de statut 
+        statut = false;
+    }
+    //test sinon statut est égal à false
+    else{
+        //récupération la valeur de la tâche (input)
+        const valeur = e.parentNode.firstChild.value;
+        //créer un nouvel élément paragraphe
+        const paragraphe = document.createElement('p');
+        //remplacer l'input par le paragraphe
+        e.parentNode.replaceChild(paragraphe, e.parentNode.firstChild);
+        //Assigner la valeur au paragraphe
+        e.parentNode.firstChild.textContent = valeur;
+        //changer la valeur de statut
+        statut = true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//</html>Exemple d'utilisation de local Storage :
+//créer des clés
+localStorage.setItem("Test", "exemple");
+localStorage.setItem("Test2", "exemple");
+localStorage.setItem("Test3", "exemple");
+//récupérer la valeur d'une clé
+console.log(localStorage.getItem("Test"));
+//mettre une clé
+localStorage.setItem("Test", 125);
+//supprimer une clé
+localStorage.removeItem("Test");
+//supprimer toutes les clés
+localStorage.clear();
