e7b1de2da0a1caefb3c7166463c1e63e4a63ce2b
jean-marie <jeanmariekouassi70@gmail.com>
2023-11-06 11:26:40 +0000

    adrar

---
+++ Mon_Projet_Fil_Rouge/PROJET FIL ROUGE/assets/js/recherche.js
@@ -0,0 +1,42 @@
+function ouvrirPage(){
+                                        var a = document.getElementById("search").value;
+                                        if (a === "Hommes") {
+                                            window.open("categorie.html#categorie1");
+                                        }
+                                        if (a === "hommes") {
+                                            window.open("categorie.html#categorie1");
+                                        }
+                                        if (a === "Homme") {
+                                            window.open("categorie.html#categorie1");
+                                        }
+                                        if (a === "homme") {
+                                            window.open("categorie.html#categorie1");
+                                        }
+                                        if (a === "Femmes") {
+                                            window.open("categorie.html#categorie2");
+                                        }
+                                         if (a === "femmes") {
+                                            window.open("categorie.html#categorie2");
+                                        }
+                                         if (a === "Femme") {
+                                            window.open("categorie.html#categorie2");
+                                        }
+                                         if (a === "femme") {
+                                            window.open("categorie.html#categorie2");
+                                        }
+                                         
+                                         if (a === "Maisons") {
+                                            window.open("categorie.html#categorie3");
+                                        }
+                                          if (a === "maisons") {
+                                            window.open("categorie.html#categorie3");
+                                        }
+                                          if (a === "Maison") {
+                                            window.open("categorie.html#categorie3");
+                                        }
+                                          if (a === "maison") {
+                                            window.open("categorie.html#categorie3");
+                                        }
+
+                                        
+                                    }
